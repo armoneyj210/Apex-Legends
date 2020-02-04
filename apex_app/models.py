91f0226c9768a8c62cac692f02fbd6e74a836adb
Joshua Armoney <armoneyj210@gmail.com>
2020-02-04 16:30:42 -0500

    models draft 1

--- apex_app/models.py
+++ apex_app/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+class Map(models.Model):
+    name = models.CharField(max_length=30)
+    map_url = models.TextField
+
+    def __str__(self):
+        return self.name
+
+
+class WeaponType(models.Model):
+    name = models.CharField(max_length= 30)
+    description = models.TextField
+
+    def __str__(self):
+        return self.name
+
+
+class Attachment(models.Model):
+    name: models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Weapon(models.Model):
+    name = models.CharField(max_length=30)
+    weapon_type = models.ForeignKey\
+        (WeaponType, on_delete=models.CASCADE, related_name='weapon_type')
+    ammo = models.CharField(max_length=20)
+    attachments: models.ForeignKey\
+        (Attachment, on_delete=models.CASCADE, related_name='weapon_attachment')
+    image_url = models.TextField
+
+    def __str__(self):
+        return self.name
+
+
+class Skin(models.Model):
+    name = models.CharField(max_length=30)
+    image_url = models.TextField
+
+    def __str__(self):
+        return self.name
+
+class Character(models.Model):
+    name = models.CharField(max_length=20)
+    passive_ability = models.CharField(max_length=30)
+    ability = models.CharField(max_length=30)
+    super = models.CharField(max_length=30)
+    image_url = models.TextField
+    background = models.TextField
+    skin = models.ForeignKey\
+        (Skin, on_delete=models.CASCADE, related_name='char_skin')
