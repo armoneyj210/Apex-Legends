99ad4969776d1716c3679887c4050f840ac6a05c
Joshua Armoney <armoneyj210@gmail.com>
2020-02-06 09:52:16 -0500

    views.py set

--- apex_app/views.py
+++ apex_app/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Skin, Character, Attachment, WeaponType, Map, Weapon
+from .serializers import SkinSerializer, AttachmentSerializer, WeaponTypeSerializer, WeaponSerializer,\
+    CharacterSerializer, MapSerializer
 
-# Create your views here.
+
+class CharacterView(viewsets.ModelViewSet):
+    queryset = Character.objects.all()
+    serializer_class = CharacterSerializer
+
+
+class SkinView(viewsets.ModelViewSet):
+    queryset = Skin.objects.all()
+    serializer_class = SkinSerializer
+
+
+class AttachmentView(viewsets.ModelViewSet):
+    queryset = Attachment.objects.all()
+    serializer_class = AttachmentSerializer
+
+
+class WeaponView(viewsets.ModelViewSet):
+    queryset = Weapon.objects.all()
+    serializer_class = WeaponSerializer
+
+
+class WeaponTypeView(viewsets.ModelViewSet):
+    queryset = WeaponType.objects.all()
+    serializer_class = WeaponTypeSerializer
+
+
+class MapView(viewsets.ModelViewSet):
+    queryset = Map.objects.all()
+    serializer_class = MapSerializer
